1f79bf5270d6e8e368e521eb916f5c65071e3f1d
malqusi <malqusi@yahoo.com>
2021-07-12 21:16:19 +0200

    nodejs week2 exercises

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -1,10 +1,67 @@
-const express = require('express')
+const express = require("express");
 const app = express();
- 
+const fs = require("fs");
+
+// Body Parser Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.get("/", function (req, res) {
+  res.send("Hello World!!");
+});
+
+app.post("/blogs", (req, res) => {
+  req.header("Content-Type");
+  const { title, content } = req.body;
+  fs.writeFileSync(title, content);
+  res.end("ok");
+});
+
+app.put("/posts/:title", (req, res) => {
+  const { title, content } = req.body;
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.end("ok");
+  } else {
+    res.status(400).json({ msg: "This post does not exist!" });
+  }
+});
+
+app.delete("/blogs/:title", (req, res) => {
+  // How to get the title from the url parameters?
+  const title = req.params.title;
+  if (fs.existsSync(title)) {
+    // Add condition here
+    fs.unlinkSync(title);
+    res.end("ok");
+  } else {
+    // Respond with message here
+    res.status(400).json({ msg: `Title not found` });
+  }
+});
+
+app.get("/blogs/:title", (req, res) => {
+  // How to get the title from the url parameters?
+  const title = req.params.title;
+  // check if post exists
+  if (fs.existsSync(title)) {
+    const post = fs.readFileSync(title);
+    // send response
+    res.json(post);
+  } else {
+    res.status(400).send(`This post does not exist!`);
+  }
+});
+
+app.get("/blogs", (req, res) => {
+  let blogs = [];
+  const arrayOfFiles = fs.readdirSync("./blogs");
+
+  arrayOfFiles.forEach((title) => {
+    blogs.push(title);
+  });
+  res.send(blogs);
+});
+app.listen(3000);
